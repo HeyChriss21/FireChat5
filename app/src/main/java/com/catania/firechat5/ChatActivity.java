0d3c15d4681024bf86aba98ba9e70f10670bf26e
HeyChriss21 <cataniach@poloagroindustriale.edu.it>
2025-04-06 17:14:26 +0200

    Codice di activity_chat.xml completo minuto 48:28

--- app/src/main/java/com/catania/firechat5/ChatActivity.java
+++ app/src/main/java/com/catania/firechat5/ChatActivity.java
@@ -7,18 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class ChatActivity extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    UserAdapter userAdapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
